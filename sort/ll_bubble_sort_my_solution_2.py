da8099627163edd91dca67f48eef5532fe09e2f8
dekelbyz <bayazi455@gmail.com>
2024-05-01 09:26:07 +0300

    My second solution for bubble sort

---
+++ sort/ll_bubble_sort_my_solution_2.py
@@ -0,0 +1,78 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    # this one is great to my opinion 
+    # less crap than the solution they suggested
+    # more readable 
+    def bubble_sort(self):
+        for _ in range(self.length - 1, 0, -1):
+            node = self.head
+            while node.next:
+                if node.value > node.next.value:
+                    node.value, node.next.value = node.next.value, node.value
+                node = node.next
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+my_linked_list.append(6)
+my_linked_list.append(5)
+my_linked_list.append(1)
+my_linked_list.append(3)
+
+print("Linked List Before Sort:")
+my_linked_list.print_list()
+
+my_linked_list.bubble_sort()
+
+print("\nSorted Linked List:")
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Linked List Before Sort:
+    4
+    2
+    6
+    5
+    1
+    3
+
+    Sorted Linked List:
+    1
+    2
+    3
+    4
+    5
+    6
+
+"""
+
