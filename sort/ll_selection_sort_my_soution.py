e2d30fe2af78a034ec1b1cd5e933215faaf81b09
dekelbyz <bayazi455@gmail.com>
2024-05-01 09:26:15 +0300

    my selection sort

---
+++ sort/ll_selection_sort_my_soution.py
@@ -0,0 +1,86 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def selection_sort(self):
+        node = self.head
+
+        while node:
+            min_node = node
+            after = node.next
+
+            while after:
+                if after.value < min_node.value:
+                    min_node = after
+                after = after.next
+            
+            if min_node != node:
+                min_node.value, node.value = node.value, min_node.value
+
+            node = node.next
+
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+my_linked_list.append(6)
+my_linked_list.append(5)
+my_linked_list.append(1)
+my_linked_list.append(3)
+
+print("Linked List Before Sort:")
+my_linked_list.print_list()
+
+my_linked_list.selection_sort()
+
+print("\nSorted Linked List:")
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Linked List Before Sort:
+    4
+    2
+    6
+    5
+    1
+    3
+
+    Sorted Linked List:
+    1
+    2
+    3
+    4
+    5
+    6
+
+"""
+
